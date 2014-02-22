c2c6996ede7966acfc7ea0126cc27d5fa6ea7f48
Jared Shumway <jaredshumway94@gmail.com>
2014-02-22 14:16:21 -0800

    Initial commit.

---
+++ cards.py
@@ -0,0 +1,82 @@
+from collections import namedtuple
+from functools import wraps
+
+Expansion = namedtuple('Expansion', ['id', 'title', 'cards'])
+
+base_cards = Expansion('DO', 'Dominion', [
+    'adventurer', 'bureaucrat', 'cellar', 'chancellor', 'chapel',
+    'council room', 'feast', 'festival', 'gardens', 'laboratory', 'library',
+    'market', 'militia', 'mine', 'moat', 'moneylender', 'remodel', 'smithy',
+    'spy', 'thief', 'throne room', 'village', 'witch', 'woodcutter', 'workshop'
+])
+
+# Dominion Core is a subset of the base set. I have removed all of the cards
+# that are generally ranked poorly or that I do not enjoy playing with
+# (f**king Woodcutter). It retains the interesting cards like Chapel and
+# Gardens.
+base_core_cards = Expansion('DC', 'Dominion Core', [
+    'cellar', 'chapel', 'council room', 'festival', 'gardens', 'laboratory',
+    'library', 'market', 'militia', 'moneylender', 'smithy', 'throne room',
+    'village', 'witch'
+])
+
+intrigue_cards = Expansion('IN', 'Intrigue', [
+    'baron', 'bridge', 'conspirator', 'coppersmith', 'courtyard', 'duke',
+    'great hall', 'harem', 'ironworks', 'masquerade', 'mining village',
+    'minion', 'nobles', 'pawn', 'saboteur', 'scout', 'secret chamber',
+    'shanty town', 'steward', 'swindler', 'torturer', 'trading post',
+    'tribute', 'upgrade', 'wishing well'
+])
+
+prosperity_cards = Expansion('PR', 'Prosperity', [
+    'bank', 'bishop', 'city', 'contraband', 'counting house', 'expand',
+    'forge', 'goons', 'grand market', 'hoard', "king's court", 'loan', 'mint',
+    'monument', 'mountebank', 'peddler', 'quarry', 'rabble', 'royal seal',
+    'talisman', 'trade route', 'vault', 'venture', 'watchtower',
+    "worker's village"
+])
+
+dark_ages_cards = Expansion('DA', 'Dark Ages', [
+    'altar', 'armory', 'band of misfits', 'bandit camp', 'beggar', 'catacombs',
+    'count', 'counterfeit', 'cultist', 'death cart', 'feodum', 'forager',
+    'fortress', 'graverobber', 'hermit', 'hunting grounds', 'ironmonger',
+    'junk dealer', 'knights', 'marauder', 'market square', 'mystic', 'pillage',
+    'poor house', 'procession', 'rats', 'rebuild', 'rogue', 'sage',
+    'scavenger', 'squire', 'storeroom', 'urchin', 'vagrant',
+    'wandering minstrel'
+])
+
+expansion_list = [
+    base_cards,
+    base_core_cards,
+    intrigue_cards,
+    prosperity_cards,
+    dark_ages_cards,
+]
+
+expansions = {expansion.id: expansion for expansion in expansion_list}
+
+
+def save(f):
+    ''' Like memoize but operates on a function of no arguments. '''
+    value = []
+
+    @wraps(f)
+    def wrap():
+        if value == []:
+            value.append(f())
+        return value[0]
+    return wrap
+
+
+@save
+def from_exp():
+    ''' Return a map of a card names to expansion ids. '''
+
+    card_from_map = {}
+
+    for expansion in expansions.itervalues():
+        for card in expansion.cards:
+            card_from_map[card] = expansion.id
+
+    return card_from_map
---
+++ kingdom.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+''' Randomly generate kingdoms for Dominion. '''
+
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+import logging
+import random
+
+import blessings
+
+import cards
+
+exclusive_sets = [
+    set(["king's court", 'throne room', 'procession']),
+    set(['bishop', 'fortress']),
+    set(['bandit camp', 'bazaar', 'fortress', 'mining village', 'village',
+        "worker's village"])
+]
+
+FAIL_THRESHOLD = 10
+
+terminal = blessings.Terminal()
+
+color_map = {
+    'DO': terminal.blue,
+    'DC': terminal.blue,
+    'IN': terminal.bright_magenta,
+    'PR': terminal.yellow,
+    'DA': terminal.bright_red,
+}
+
+
+def color(expansion, string):
+    return color_map[expansion](string)
+
+
+def expansion_help():
+    output = []
+
+    for i, expansion in enumerate(cards.expansion_list):
+        exp_string = color(
+            expansion.id, '%s - %s' % (expansion.id, expansion.title))
+
+        if not i % 2:
+            output.append('\t%s\t\t' % exp_string)
+        else:
+            output.append('%s\n' % exp_string)
+
+    return ''.join(output)
+
+
+def display_set(kingdom, script_output):
+    ''' Print out all of the cards in |kingdom|. '''
+
+    def sort_key(card):
+        return (cards.from_exp()[card], card)
+
+    for i, card in enumerate(sorted(kingdom, key=sort_key)):
+        if script_output:
+            print '%s %s' % (cards.from_exp()[card], card)
+        else:
+            print '  %2d  %s' % (i + 1, color(cards.from_exp()[card], card))
+
+
+def check_exclusive_sets(kingdom):
+    for eset in exclusive_sets:
+        if len(eset & kingdom) > 1:
+            return False
+
+    return True
+
+
+def generate_kingdom(card_pool, kingdom_size, required):
+    failures = 0
+
+    while failures < FAIL_THRESHOLD:
+        kingdom = set(random.sample(card_pool, kingdom_size))
+
+        if not check_exclusive_sets(kingdom):
+            failures += 1
+            continue
+
+        return required | kingdom
+
+    logging.error('failed to generate a kingdom :(')
+
+
+def create_kingdom(args):
+    ''' Generate a random kingdom as specified by args. '''
+
+    def parse_list_string(lstr):
+        return set(s.strip() for s in lstr.split(',') if s)
+
+    if not args.expansions:
+        expansions = cards.expansions.keys()
+        expansions.remove('DO')
+    else:
+        expansions = parse_list_string(args.expansions)
+
+    kingdom_size = args.size
+    required = parse_list_string(args.require)
+    forbidden = parse_list_string(args.forbid)
+    card_pool = set()
+
+    for expansion_id in expansions:
+        card_pool.update(cards.expansions[expansion_id].cards)
+
+    card_pool.difference_update(forbidden)
+    card_pool.difference_update(required)
+
+    return generate_kingdom(card_pool, kingdom_size, required)
+
+
+def main():
+    help_text = '%s\n\n%s' % (__doc__.strip(), expansion_help())
+
+    parser = ArgumentParser(
+        description=help_text,
+        formatter_class=RawDescriptionHelpFormatter)
+
+    parser.add_argument(
+        'size', default=10, type=int, nargs='?',
+        help='size of kingdom to generate')
+    parser.add_argument(
+        '-e', '--expansions', type=str, metavar='LIST',
+        help='comma seperated list of expansions to draw from')
+    parser.add_argument(
+        '-r', '--require', type=str, default='', metavar='LIST',
+        help='comma seperated list of cards to include')
+    parser.add_argument(
+        '-f', '--forbid', type=str, default='', metavar='LIST',
+        help='comma seperated list of cards to ignore')
+    parser.add_argument(
+        '-s', '--script-output', action='store_true',
+        help='format output in a script friendly way')
+
+    args = parser.parse_args()
+    display_set(create_kingdom(args), args.script_output)
+
+
+if __name__ == '__main__':
+    main()
